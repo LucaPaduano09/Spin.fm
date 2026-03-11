ac670114fc595ed30578eb7437c6296b1b553509
Luca Paduano <lucapaduano09@outlook.it>
2026-03-11 08:27:43 +0100

    fix on supabase-server types

--- src/lib/supabase-server.ts
+++ src/lib/supabase-server.ts
@@ -9,7 +9,7 @@ export async function createServerSupabaseClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name)              { return cookieStore.get(name)?.value },
+        get(name:string)              { return cookieStore.get(name)?.value },
         set(name: string, value: string, opts: Record<string, unknown>) { try { cookieStore.set({ name, value, ...opts }) } catch {} },
         remove(name: string, opts: Record<string, unknown>)             { try { cookieStore.set({ name, value: '', ...opts }) } catch {} },
       },
